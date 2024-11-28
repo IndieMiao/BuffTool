df562146b5bb4a46de361704af2ef5658eb37673
zhangshaojie <zsjasper@126.com>
2024-11-28 08:35:35 +0800

    clean

--- BuffTool.lua
+++ BuffTool.lua
@@ -4,25 +4,30 @@ local auraTexturesByName = {
         texture = 'Interface\\AddOns\\BuffTool\\Images\\Aura176',
         x = -50, -- 假设的X坐标
         y = -20, -- 假设的Y坐标
-        alpha = 0.4, -- 假设的透明度
+        alpha = 0.7, -- 假设的透明度
         width = 256, -- 假设的宽度
         height = 256, -- 假设的高度
-        Blend = "ADD",
+        Blend = "BLEND",
         Pos = "LEFT"
     },
-    ['Water Shield'] = {
+    ['The Eye of Diminution'] = {
         texture = 'Interface\\AddOns\\BuffTool\\Images\\Aura179',
         x = 0, -- 另一个X坐标
         y = -30, -- 另一个Y坐标
         alpha = 0.75, -- 另一个透明度
-        width = 360, -- 另一个宽度
-        height = 360, -- 另一个高度
+        width = 300, -- 另一个宽度
+        height = 300, -- 另一个高度
         Blend = "BLEND",
         Pos = "CENTER"
     }
 }
+
+-- Electrified
+-- Clearcasting
+-- Lighting Speed
+-- Stormcaller's Warth
 local isDebug=true
-local isDebugTexture= false
+local isDebugTexture= true
 local auraTexturesObjects = {}
 
 local buffToolFrame = CreateFrame('FRAME')
@@ -31,18 +36,20 @@ buffToolFrame:SetWidth(200)
 buffToolFrame:SetHeight(200)
 buffToolFrame:RegisterEvent('COMBAT_TEXT_UPDATE') -- 监听光环应用事件
 
-function DebugAllBuffTexture()
-for spellName, auraInfo in pairs(auraTexturesByName) do
-    local textureObject = buffToolFrame:CreateTexture(nil, 'ARTWORK')
-    textureObject:SetTexture(auraInfo.texture)
-    textureObject:SetPoint('CENTER', buffToolFrame, auraInfo.Pos, auraInfo.x, auraInfo.y)
-    textureObject:SetAlpha(auraInfo.alpha)
-    textureObject:SetWidth(auraInfo.width)
-    textureObject:SetHeight(auraInfo.height)
-    textureObject:SetBlendMode(auraInfo.Blend)
-    textureObject:Show() -- 初始时隐藏
-    auraTexturesObjects[spellName] = textureObject
-end
+local function DebugAllBuffTexture()
+    for spellName, auraInfo in pairs(auraTexturesByName) do
+        if not auraTexturesObjects[spellName] then
+            local textureObject = buffToolFrame:CreateTexture(nil, 'ARTWORK')
+            textureObject:SetTexture(auraInfo.texture)
+            textureObject:SetPoint('CENTER', buffToolFrame, auraInfo.Pos, auraInfo.x, auraInfo.y)
+            textureObject:SetAlpha(auraInfo.alpha)
+            textureObject:SetWidth(auraInfo.width)
+            textureObject:SetHeight(auraInfo.height)
+            textureObject:SetBlendMode(auraInfo.Blend)
+            textureObject:Show()
+            auraTexturesObjects[spellName] = textureObject
+        end
+    end
 end
 
 local function HandleAuraByName(spellName, isActive)
@@ -85,4 +92,3 @@ buffToolFrame:SetScript('OnEvent', function()
 end)
 
 if isDebugTexture then DebugAllBuffTexture()end
-
